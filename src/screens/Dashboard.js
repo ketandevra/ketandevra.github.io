129645b3d1fc1017f6edeaa1cd63919093b8b89f
ketandevra <ketandeora3@gmail.com>
2024-12-08 15:14:27 +0530

    Add files via upload
    First Commit

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+
+const Dashbaord = () => {
+  return (
+    <div style={{}}>
+      <Typography style={{ fontFamily: 'cursive', fontSize: 60, fontWeight: 600 }}>
+        Ghanchi Samaj Pali Rajasthan
+      </Typography>
+    </div>
+  );
+};
+
+export default Dashbaord;
